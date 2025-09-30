6d21cc339df56709a594a84ee4031584acf58060
lrj <LiRongji1995@gmail.com>
2025-09-30 11:14:59 +0800

    新增： 3Sum.go
    修改：
    Longest_Common_Prefix.go
    
    删除：

---
+++ DataStructure-And-Algorithm/Phase 1/Array/3Sum.go
@@ -0,0 +1,8 @@
+package Array
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+
+}
--- DataStructure-And-Algorithm/Phase 1/Array/Longest_Common_Prefix.go
+++ DataStructure-And-Algorithm/Phase 1/Array/Longest_Common_Prefix.go
@@ -1,1 +1,22 @@
 package Array
+
+import "sort"
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	sort.Strings(strs) //按字典序升序排列
+	start := strs[0]
+	end := strs[len(strs)-1]
+	res := ""
+
+	for i := 0; i < len(start); i++ {
+		if i < len(end) && start[i] == end[i] {
+			res += string(start[i]) //start[i] 的类型是 byte
+		} else {
+			break
+		}
+	}
+	return res
+}

d686384b1355408fc2d771afa8d7f8b0de01a646
lrj <LiRongji1995@gmail.com>
2025-10-22 11:32:06 +0800

    新增：
    修改：
    Merge_Sorted_Array.go
    
    删除：

---
+++ DataStructure-And-Algorithm/Phase 1/Array/Merge_Sorted_Array.go
@@ -0,0 +1,16 @@
+package Array
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for o := m + n; m > 0 && n > 0; o-- {
+		if nums1[m-1] <= nums2[n-1] {
+			nums1[o-1] = nums2[n-1]
+			n--
+		} else {
+			nums1[o-1] = nums1[m-1]
+			m--
+		}
+	}
+	for ; n > 0; n-- {
+		nums1[n-1] = nums2[n-1]
+	}
+}

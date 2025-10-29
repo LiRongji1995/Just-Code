298232519c46a15b8e49ef73580443ac1309c948
lrj <LiRongji1995@gmail.com>
2025-10-29 15:14:36 +0800

    新增： Contains_DuplicateII.go
    修改：
    
    删除：

--- DataStructure-And-Algorithm/Phase 1/Hash Table/Contains_DuplicateII.go
+++ DataStructure-And-Algorithm/Phase 1/Hash Table/Contains_DuplicateII.go
@@ -1,1 +1,24 @@
 package Hash_Table
+
+/*
+只要关心：有没有两个下标 i、j，使得
+
+nums[i] == nums[j]
+
+|i - j| <= k
+
+可以一边从左到右扫数组，一边用一个 map 记录“这个数上次出现的下标”。
+*/
+func containsNearbyDuplicate(nums []int, k int) bool {
+	lastIndex := make(map[int]int)
+
+	for i, v := range nums {
+		if prev, ok := lastIndex[v]; ok {
+			if i-prev <= k {
+				return true
+			}
+		}
+		lastIndex[v] = i
+	}
+	return false
+}

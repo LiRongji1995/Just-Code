19fe81312dd86953bb20527bceffac1d2627fee7
lrj <LiRongji1995@gmail.com>
2025-10-29 16:37:24 +0800

    新增： 修改： Word_Pattern.go 删除：

--- DataStructure-And-Algorithm/Phase 1/Hash Table/Word_Pattern.go
+++ DataStructure-And-Algorithm/Phase 1/Hash Table/Word_Pattern.go
@@ -1,1 +1,36 @@
 package Hash_Table
+
+import "strings"
+
+func wordPattern(pattern string, s string) bool {
+	words := strings.Fields(s)
+
+	if len(words) != len(pattern) {
+		return false
+	}
+
+	charToWord := make(map[byte]string)
+	wordToChar := make(map[string]byte)
+
+	for i := range len(pattern) {
+		p := pattern[i]
+		w := words[i]
+
+		if mappedWord, ok := charToWord[p]; ok {
+			if mappedWord != w {
+				return false
+			}
+		} else {
+			charToWord[p] = w
+		}
+
+		if mappedChar, ok := wordToChar[w]; ok {
+			if mappedChar != p {
+				return false
+			}
+		} else {
+			wordToChar[w] = p
+		}
+	}
+	return true
+}

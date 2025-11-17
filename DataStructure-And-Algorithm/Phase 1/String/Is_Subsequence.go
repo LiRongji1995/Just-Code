74a3ed3806d00d08a3dd5320cc3718604f52e172
lrj <LiRongji1995@gmail.com>
2025-11-17 16:27:03 +0800

    新增： Is_Subsequence.go 修改： 删除：

--- DataStructure-And-Algorithm/Phase 1/String/Is_Subsequence.go
+++ DataStructure-And-Algorithm/Phase 1/String/Is_Subsequence.go
@@ -2,12 +2,11 @@ package String
 
 func isSubsequence(s string, t string) bool {
 	i, j := 0, 0
-	for i < len(s) && j < len(t) {
+	if i < len(s) && j < len(t) {
 		if s[i] == t[j] {
 			i++
 		}
 		j++
 	}
-	//如果 i == len(s)，说明 s 的每个字符都按顺序在 t 里找到了
 	return i == len(s)
 }
